a3c0c07cfca25954596f7436eaabe195086636e9
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-02-21 19:07:24 +0100

    Update view of typography page

--- lib/pages/typography_page.dart
+++ lib/pages/typography_page.dart
@@ -11,73 +11,80 @@ class TypographyPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(Routes.typography.name),
       ),
-      body: ListView(
-        children: [
-          Text(
-            'Display Large',
-            style: Theme.of(context).textTheme.displayLarge,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  'Display Large',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                Text(
+                  'Display Medium',
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                Text(
+                  'Display Small',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                const Divider(),
+                Text(
+                  'Headline Large',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                Text(
+                  'Headline Medium',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  'Headline Small',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Divider(),
+                Text(
+                  'Title Large',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  'Title Medium',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Text(
+                  'Title Small',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const Divider(),
+                Text(
+                  'Body Large',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                  'Body Medium',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                Text(
+                  'Body Small',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const Divider(),
+                Text(
+                  'Label Large',
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                Text(
+                  'Label Medium',
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+                Text(
+                  'Label Small',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+              ],
+            ),
           ),
-          Text(
-            'Display Medium',
-            style: Theme.of(context).textTheme.displayMedium,
-          ),
-          Text(
-            'Display Small',
-            style: Theme.of(context).textTheme.displaySmall,
-          ),
-          const Divider(),
-          Text(
-            'Headline Large',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          Text(
-            'Headline Medium',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          Text(
-            'Headline Small',
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-          const Divider(),
-          Text(
-            'Title Large',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          Text(
-            'Title Medium',
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          Text(
-            'Title Small',
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          const Divider(),
-          Text(
-            'Body Large',
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          Text(
-            'Body Medium',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          Text(
-            'Body Small',
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
-          const Divider(),
-          Text(
-            'Label Large',
-            style: Theme.of(context).textTheme.labelLarge,
-          ),
-          Text(
-            'Label Medium',
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-          Text(
-            'Label Small',
-            style: Theme.of(context).textTheme.labelSmall,
-          ),
-        ],
+        ),
       ),
     );
   }
